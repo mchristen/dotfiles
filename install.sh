b9ecd260f5d8aaeb35e25a350a363dd60ab94047
Matt Christen <matt@resly.com>
2017-09-03 22:09:05 -0500

    Added some more dev prereqs; Add ruby, node and python default version support

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ~/
@@ -31,6 +29,51 @@ if [ ! -d ~/.pyenv ]; then
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 fi
 
+. ~/.local_home/.rc
+
+RUBY_VERSION="2.3.4"
+read -p "Which version of ruby do you want to use account wide?($RUBY_VERSION)" choice
+if [ "" != "$choice" ]; then
+    RUBY_VERSION=$choice
+fi
+
+
+NODE_VERSION="8.1.3"
+read -p "Which version of node do you want to use account wide?($NODE_VERSION)" choice
+if [ "" != "$choice" ]; then
+    NODE_VERSION=$choice
+fi
+
+
+PYTHON_VERSION="3.6.2"
+read -p "Which version of python do you want to use account wide?($PYTHON_VERSION)" choice
+if [ "" != "$choice" ]; then
+    PYTHON_VERSION=$choice
+fi
+
+rbenv install --list | grep $RUBY_VERSION
+if [ "0" == "$?" ]; then
+    rbenv install $RUBY_VERSION
+    rbenv global $RUBY_VERSION
+else
+    echo "Ruby version not found; try updating ruby-install"
+fi
+
+nodenv install --list | grep $NODE_VERSION
+if [ "0" == "$?" ]; then
+    nodenv install $NODE_VERSION
+    nodenv global $NODE_VERSION
+else
+    echo "Node version not found; try updating nodenv"
+fi
+
+pyenv install --list | grep $PYTHON_VERSION
+if [ "0" == "$?" ]; then
+    pyenv install $PYTHON_VERSION
+    pyenv global $PYTHON_VERSION
+else
+    echo "Python version not found; try updating pyenv"
+fi
 
 if [ ! -e ~/.local_home ]; then
 	ln -s $SOURCE_DIR ~/.local_home
--- prereqs.sh
+++ prereqs.sh
@@ -4,4 +4,4 @@ set -x
 
 cd ~/
 
-sudo apt-get install build-essential vim autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev 
+sudo apt-get install build-essential vim autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev libbz2-dev libsqlite3-dev
