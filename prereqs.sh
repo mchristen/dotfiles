04da7692a55852b084ce93db4e04e2d6b8645ffe
Matt Christen <matt@resly.com>
2017-09-03 22:49:33 -0500

    Reformat prereq file

--- prereqs.sh
+++ prereqs.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-set -x
-
-cd ~/
-
-sudo apt-get install build-essential vim autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev libbz2-dev libsqlite3-dev
+sudo apt-get install \
+    autoconf \
+    bison \
+    build-essential \
+    gcc-6 \
+    libbz2-dev \
+    libffi-dev \
+    libgdbm3 \
+    libgdbm-dev \
+    libncurses5-dev \
+    libreadline6-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libyaml-dev \
+    tmux \
+    vim \
+    zlib1g-dev \
