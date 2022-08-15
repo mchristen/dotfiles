038385586e0a4ffd41a042f7ece6914ebfd19966
Matt Christen <1231534+mchristen@users.noreply.github.com>
2022-08-15 15:33:08 -0500

    asdf/direnv

--- install.sh
+++ install.sh
@@ -11,22 +11,8 @@ fi
 $SOURCE_DIR/prereqs.sh
 
 
-if [ ! -d ~/.rbenv ]; then
-    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-    cd ~/.rbenv && src/configure && make -C src
-    cd ~/
-    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-fi
-
-if [ ! -d ~/.nodenv ]; then
-    git clone https://github.com/nodenv/nodenv.git ~/.nodenv
-    cd ~/.nodenv && src/configure && make -C src
-    cd ~/
-    git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
-fi
-
-if [ ! -d ~/.pyenv ]; then
-    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+if [ ! -d ~/.asdf ]; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 fi
 
 if [ ! -e ~/.local_home ]; then
@@ -35,53 +21,26 @@ if [ ! -e ~/.local_home ]; then
 	echo ". ~/.local_home/.rc" >> ~/.bashrc
 fi
 
-RUBY_VERSION="2.3.4"
-read -p "Which version of ruby do you want to use account wide?($RUBY_VERSION)" choice
-if [ "" != "$choice" ]; then
-    RUBY_VERSION=$choice
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add python
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git
+
+asdf install nodejs latest
+asdf install ruby latest
+asdf install python latest
+asdf install golang latest
+asdf install deno latest
+
+if [ ! -e ~/.tool-versions ]; then
+	echo "direnv system" >> ~/.tool-versions
 fi
 
+mkdir -p .local/share/bash_completion.d
 
-NODE_VERSION="8.1.3"
-read -p "Which version of node do you want to use account wide?($NODE_VERSION)" choice
-if [ "" != "$choice" ]; then
-    NODE_VERSION=$choice
+if ! grep deno.bash ~/.bashrc ; then
+	asdf shell deno latest
+	deno completions bash > ~/.local/share/bash_completion.d/deno.bash
+	echo "source $HOME/.local/share/bash_completion.d/deno.bash" >> ~/.bashrc
 fi
-
-
-PYTHON_VERSION="3.6.2"
-read -p "Which version of python do you want to use account wide?($PYTHON_VERSION)" choice
-if [ "" != "$choice" ]; then
-    PYTHON_VERSION=$choice
-fi
-
-rbenv install --list | grep $RUBY_VERSION
-if [ "0" == "$?" ]; then
-    rbenv install $RUBY_VERSION
-    rbenv global $RUBY_VERSION
-else
-    echo "Ruby version not found; try updating ruby-install"
-fi
-
-nodenv install --list | grep $NODE_VERSION
-if [ "0" == "$?" ]; then
-    nodenv install $NODE_VERSION
-    nodenv global $NODE_VERSION
-else
-    echo "Node version not found; try updating nodenv"
-fi
-
-pyenv install --list | grep $PYTHON_VERSION
-if [ "0" == "$?" ]; then
-    pyenv install $PYTHON_VERSION
-    pyenv global $PYTHON_VERSION
-else
-    echo "Python version not found; try updating pyenv"
-fi
-
-if [ ! -e ~/.local_home ]; then
-	ln -s $SOURCE_DIR ~/.local_home
-	echo ". ~/.local_home/.rc" >> ~/.bashrc
-fi
-
-gem install tmuxinator
--- prereqs.sh
+++ prereqs.sh
@@ -4,17 +4,36 @@ sudo apt-get install \
     autoconf \
     bison \
     build-essential \
-    gcc-6 \
+    ca-certificates \
+    coreutils \
+    curl \
+    direnv \
+    dirmngr \
+    gawk \
+    gnupg \
+    gpg \
     inotify-tools \
     libbz2-dev \
+    libdb-dev \
     libffi-dev \
-    libgdbm3 \
     libgdbm-dev \
+    libgdbm6 \
+    liblzma-dev \
     libncurses5-dev \
-    libreadline6-dev \
+    libncursesw5-dev \
+    libpq-dev \
+    libpq5 \
+    libreadline-dev \
     libsqlite3-dev \
     libssl-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
     libyaml-dev \
+    lsb-release
+    tk-dev \
     tmux \
+    unzip \
     vim \
+    xz-utils \
+    zip \
     zlib1g-dev \
